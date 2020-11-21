5cf7f25027fe2f872f85365e1da17540d801a9df
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-11-20 18:14:13 -0600

    Search bar + header in position

---
+++ ip-address-tracker-master/ip/src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Search() {
+	return (
+		<div>
+			<div className="header__search">
+				<input className="header__searchInput" type="text" />
+			</div>
+		</div>
+	);
+}
+
+export default Search;
